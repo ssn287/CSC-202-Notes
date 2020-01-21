493719c2e48aa8cbf974eea1ebe193239ff93e09
Neal <ssn287@email.vccs.edu>
2020-01-21 15:47:16 -0500

    second commit

--- src/LinearList.java
+++ src/LinearList.java
@@ -11,6 +11,14 @@ public class LinearList {
 	}
 	public Node head = null;
 	public Node tail = null;
+//	public void initializeList(int item) {
+//		Node current = head;
+//		while(item > 0) {
+//			current.data = (int)(Math.random() * 100);
+//			current = current.next;
+//			item--;
+//		}
+//	}
 	public void InsertNode(int d) {
 		//create a new Node
 		Node newNode = new Node(d);
@@ -36,16 +44,47 @@ public class LinearList {
 			System.out.print(" " + current.data);
 			current = current.next;
 		}
+	}
+	public double sum() {
+		Node current = head; double sum = 0;
+		if(head == null) {
+			System.out.print("\n\tThe List is Empty");
+			return 0;
+		}
+		while(current != null) {
+			sum += current.data;
+			current = current.next;
+		}
 		System.out.println();
+		return sum;
+	}
+	public int largest() {
+		Node current = head;
+		int large = current.data;
+		if(head == null) {
+			System.out.print("\n\tThe List is Empty");
+			return 0;
+		}
+		while(current != null) {
+			if(current.data >= large) {
+				large = current.data;
+			}
+			current = current.next;
+		}
+		System.out.println();
+		return large;
 	}
 	public static void main(String[] args) {
 		LinearList slist = new LinearList();
 		int num; Scanner kybd = new Scanner(System.in);
+//		slist.initializeList(5);
 		while(true) {
 			int choice = Integer.parseInt(JOptionPane.showInputDialog("\n\tMake a Selection from the following: "
 					+ "\n\t\t1 to Insert into the List"
 					+ "\n\t\t2 to Print the Linear List"
-					+ "\n\t\t3 to Exit Program"));
+					+ "\n\t\t3 to Calculate the Sum"
+					+ "\n\t\t4 to Find the Largest Element"
+					+ "\n\t\t5 to Exit Program"));
 			switch(choice) {
 			case 1:
 				System.out.print("\n\tEnter an Integer Number (e.g. 34): ");
@@ -56,6 +95,12 @@ public class LinearList {
 				slist.display();
 				break;
 			case 3:
+				System.out.print("\n\tThe Sum of the List Elements is: " + slist.sum());
+				break;
+			case 4:
+				System.out.print("\n\tThe Largest Element is: " + slist.largest());
+				break;
+			case 5:
 				System.exit(0);
 			default:
 				System.out.println("\n\tInvalid Input (Re-Enter Value): ");
